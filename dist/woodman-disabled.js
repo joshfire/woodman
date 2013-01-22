fd91a20b7518055155811c1d57caa16fe3a77307
François Daoust <francois@joshfire.com>
2013-01-22 11:33:18 +0100

    Version 0.1.3

---
+++ dist/woodman-disabled.js
@@ -0,0 +1,4 @@
+/*! Woodman - v0.1.3 - 2013-01-22 - https://github.com/joshfire/woodman
+Copyright 2013 Joshfire; MIT license */
+
+(function(e,t){var n=null,r=function(e,t,r){n=r()};r("woodman-disabled",[],function(){var e=function(){},t=function(){arguments&&arguments.length>0&&arguments[arguments.length-1]()};return{registerAppender:e,registerLayout:e,load:t,unload:t,initialize:t,start:t,stop:e,getLogger:function(){return{log:e,info:e,warn:e,error:e}}}}),t?t("woodman",n):typeof module!="undefined"&&module.exports&&(t=require("amdefine")(module),t(n)),e&&(e.woodman=n)})(typeof window!="undefined"?window:this,typeof define=="function"?define:null);
