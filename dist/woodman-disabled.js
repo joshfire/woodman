dfea6fd25cb7e49a6d4bc7ec1ca70c892b9418c3
François Daoust <francois@joshfire.com>
2013-05-05 18:14:56 +0200

    v0.2.0
    Changelog since v0.1.9:
    - Feature: String parameters substitution support: log('H{}!', 'ey');
    - Feature: Decision to use "toString" based on method overloading
    - Feature: PatternLayout supports non abbreviated conversion patterns
    - Feature: Log event filtering supported, RegexFilter created
    - Chore: switched to require.js shortcut notation
    - Bug fix: JSONLayout did not return the right object structure

--- dist/woodman-disabled.js
+++ dist/woodman-disabled.js
@@ -1,6 +1,6 @@
-/*! Woodman - v0.1.9 - 2013-05-02 - https://github.com/joshfire/woodman
+/*! Woodman - v0.2.0 - 2013-05-05 - https://github.com/joshfire/woodman
 Copyright 2013 Joshfire; MIT license */
 
 /* Disabled distribution */
 
-(function(e,t){var n=null,r=function(e,t,r){n=r()};r("woodman-disabled",[],function(){var e=function(){},t=function(){arguments&&arguments.length>0&&arguments[arguments.length-1]()};return{registerAppender:e,registerLayout:e,load:t,unload:t,initialize:t,start:t,stop:e,getLogger:function(){return{log:e,info:e,warn:e,error:e}}}}),t?t("woodman",n):typeof module!="undefined"&&module.exports&&(t=require("amdefine")(module),t(n)),e&&(e.woodman=n)})(typeof window!="undefined"?window:this,typeof define=="function"?define:null);
+(function(e,t){var n=null,r=function(e,t,r){n=r()};r("woodman-disabled",[],function(){var e=function(){},t=function(){arguments&&arguments.length>0&&arguments[arguments.length-1]()};return{registerAppender:e,registerFilter:e,registerLayout:e,load:t,unload:t,initialize:t,start:t,stop:e,getLogger:function(){return{log:e,info:e,warn:e,error:e}}}}),t?t("woodman",n):typeof module!="undefined"&&module.exports&&(t=require("amdefine")(module),t(n)),e&&(e.woodman=n)})(typeof window!="undefined"?window:this,typeof define=="function"?define:null);
