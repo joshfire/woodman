8cb74628504ea25b28888eab18be52c02cdac9d6
François Daoust <francois@joshfire.com>
2013-06-26 23:01:24 +0200

    v0.8.0
    Changelog since v0.7.0:
    - "registerLevel" creates trace functions on existing loggers (#16)
    - Added JUnit XML report file generation to test runner.

--- dist/woodman-disabled.js
+++ dist/woodman-disabled.js
@@ -1,5 +1,5 @@
 /*!
-Woodman - v0.7.0 - 2013-06-08
+Woodman - v0.8.0 - 2013-06-26
  Copyright 2013 Joshfire; MIT license
  https://github.com/joshfire/woodman
 
@@ -13,4 +13,4 @@ Built with RequireJS optimizer r.js 2.1.2
  new BSD or MIT licensed
  http://github.com/jrburke/requirejs
 */
-(function(e,t){var n=null,r=function(e,t,r){n=r()};r("woodman-disabled",[],function(){var e=function(){},t=function(){arguments&&arguments.length>0&&arguments[arguments.length-1]()};return{registerAppender:e,registerFilter:e,registerLayout:e,load:t,unload:t,initialize:t,start:t,stop:e,getLogger:function(){return{log:e,info:e,warn:e,error:e}}}}),t?t("woodman",n):typeof module!="undefined"&&module.exports&&(t=require("amdefine")(module),t(n)),e&&(e.woodman=n)})(typeof window!="undefined"?window:this,typeof define=="function"?define:null);
+(function(e,t){var n=null,r=function(e,t,r){n=r()};r("woodman-disabled",[],function(){var e=function(){},t=function(){arguments&&arguments.length>0&&arguments[arguments.length-1]()};return{registerAppender:e,registerFilter:e,registerLayout:e,registerLevel:e,registerStandardLevels:e,load:t,unload:t,initialize:t,start:t,stop:e,getLogger:function(){return{log:e,info:e,warn:e,error:e}}}}),t?t("woodman",n):typeof module!="undefined"&&module.exports&&(t=require("amdefine")(module),t(n)),e&&(e.woodman=n)})(typeof window!="undefined"?window:this,typeof define=="function"?define:null);
